4672553e7d0cda7e1afe429394895d7bf8a8a5d4
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-06-17 10:44:12 +0000

    Palindrome checking program

---
+++ PalindromeChecker.java
@@ -0,0 +1,38 @@
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        // goal is to check first and last character, then second and second last, and so on match
+
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scan.nextLine();
+
+        // goal is to have a while loop until string exists
+        
+        int low = 0;
+        int high = input.length()-1;
+
+        // assume entered string is not a palindrome
+        boolean isPalindrome = false;
+
+        // loop until low is less than high
+        while(low<high){
+            if (input.charAt(low) == input.charAt(high)){
+                isPalindrome = true;
+                break;
+            }
+        low ++; // increment low pointer
+        high --; // decrement high pointer
+
+        }
+
+        if (isPalindrome){
+            System.out.printf("The %s is a palindrome.",input);
+        } else {
+            System.out.printf("The %s is not a palindrome.",input);
+        }
+
+
+
+    }
+    
+}
