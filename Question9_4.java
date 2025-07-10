8bd5c326ee1f68b9da6f79484d2779e4aa7a7125
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-07-10 08:59:22 +0000

    9.4

---
+++ Question9_4.java
@@ -0,0 +1,11 @@
+public class Question9_4 {
+    public static void main (String[]args){
+        java.util.Random random = new java.util.Random(1000); // seed with 1000. 
+        
+        // displays the first 50 random integers between 0 and 100 
+        for (int i = 0; i < 50; i++){
+            System.out.println(random.nextInt(100));
+        }
+
+    }
+}
