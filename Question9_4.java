d726e5aecdaa364f278c48eb9cae97fd30ec99f0
Nalin Narayan <160528770+NalNar@users.noreply.github.com>
2025-07-19 23:46:01 -0400

    Update Question9_4.java

--- Question9_4.java
+++ Question9_4.java
@@ -1,3 +1,4 @@
+// program to display first 50 random integers between 0 and 100
 public class Question9_4 {
     public static void main (String[]args){
         java.util.Random random = new java.util.Random(1000); // seed with 1000. 
